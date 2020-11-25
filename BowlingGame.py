f3a949a7c927cc08c3ebb2b417935f452f6b970e
Dan Hayworth <dan.d.hayworth@gmail.com>
2020-11-25 22:24:23 +1300

    Start

---
+++ BowlingGame.py
@@ -0,0 +1,21 @@
+class BowlingGame(object):
+    def __init__(self):
+        self.throw= []
+        self.score= 0
+
+    def throw(self,pins):
+        self.throw.append(pins)
+
+
+    def calculate_score(self):
+        ball = 0
+        for frames in range(10):
+            if self.throws[ball]==10:
+                self.score +=10 + self.throws[ball+1] + self.throws[ball +2]
+                ball += 1
+            elif self.throw[ball] + self.throws[ball+1] == 10:
+                self.score += 10 + self.throws [ball +2]
+                ball +=2
+            else:
+                self.score += self.throws[ball] + self.throw[ball + 1]
+                ball += 2

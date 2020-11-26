bf12fe39e78a56f89598143f6b64b10150011991
Dan Hayworth <dan.d.hayworth@gmail.com>
2020-11-26 13:28:23 +1300

    added PythonDoc comments

--- BowlingGameTest.py
+++ BowlingGameTest.py
@@ -6,6 +6,11 @@ class BowlingGameTests(unittest.TestCase):
 
     # adding a test method to test all gutters
     def test_all_gutters(self):
+        """
+        @:parameter 20 times throw
+        @:parameter 0 pins hit
+        @:returns 0 score
+        """
         game = BowlingGame()
         game.throw_many(20, 0)
         game.calculate_score()
@@ -13,6 +18,11 @@ class BowlingGameTests(unittest.TestCase):
 
     # adding a test method to test a perfect game
     def test_perfect_game(self):
+        """
+        @:parameter 12 times throw
+        @:parameter 10 pins hit
+        @:returns 300 score
+        """
         game = BowlingGame()
         game.throw_many(12, 10)
         game.calculate_score()
@@ -20,6 +30,11 @@ class BowlingGameTests(unittest.TestCase):
 
     # adding a test method for throw all with 1 pin hit always
     def test_all_ones(self):
+        """
+        @:parameter 20 times throw
+        @:parameter 1 pins hit
+        @:returns 20 score
+        """
         game = BowlingGame()
         number_of_times = 20
         pins = 1
@@ -29,6 +44,11 @@ class BowlingGameTests(unittest.TestCase):
 
     # adding a test method for different types of throws
     def test_different_throws(self):
+        """
+        @:parameter 5 times single throw
+        @:parameter different numbers of pins hit
+        @:returns 15 score
+        """
         game = BowlingGame()
         game.throw_one(6)
         game.throw_one(0)
@@ -42,6 +62,11 @@ class BowlingGameTests(unittest.TestCase):
 
     # adding a test method for what is meant to be a spare
     def test_for_spare(self):
+        """
+        @:parameter 4 times single throw spare
+        @:parameter different number of pins hit
+        @:returns 24 score
+        """
         game = BowlingGame()
         game.throw_one(4)
         game.throw_one(6)
@@ -54,6 +79,11 @@ class BowlingGameTests(unittest.TestCase):
 
     # adding test method to test the strike
     def test_for_strike(self):
+        """
+        @:parameter 3 times throw including strike
+        @:parameter different number of pins hit
+        @:returns 22 score
+        """
         game = BowlingGame()
         game.throw_one(10)
         game.throw_one(4)
